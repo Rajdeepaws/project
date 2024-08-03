48674af4a905504052886ea899daf7f0e952ac68
raja <raja@gmail.com>
2024-08-04 01:39:06 +0530

    3rd commit

---
+++ terraform_projects/main.tf
@@ -0,0 +1,8 @@
+provider "aws" {
+    region = "us-east-1"  # Set your desired AWS region
+}
+
+resource "aws_instance" "example" {
+    ami           = "ami-0a0e5d9c7acc336f1"
+    instance_type = "t2.micro"
+}
